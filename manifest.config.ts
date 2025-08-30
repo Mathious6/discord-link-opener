d6bc373b07e21a9885e1a34ab7551b84dc423a24
Mathious6 <mathious61@gmail.com>
2025-08-30 09:32:30 +0000

    feat: update manifest and assets for Discord link opener

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,28 +1,29 @@
-import { defineManifest } from '@crxjs/vite-plugin'
-import pkg from './package.json'
+import { defineManifest } from "@crxjs/vite-plugin";
+import pkg from "./package.json";
 
 export default defineManifest({
   manifest_version: 3,
   name: pkg.name,
+  description: "Automatically monitor and open Discord links in your browser.",
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    16: "public/icons/logo16.png",
+    32: "public/icons/logo32.png",
+    48: "public/icons/logo48.png",
+    128: "public/icons/logo128.png",
   },
-  action: {
-    default_icon: {
-      48: 'public/logo.png',
+  action: {},
+  background: {
+    service_worker: "src/background.ts",
+  },
+  permissions: ["sidePanel", "contentSettings", "tabs"],
+  content_scripts: [
+    {
+      js: ["src/content/main.tsx"],
+      matches: ["https://discord.com/*"],
     },
-    default_popup: 'src/popup/index.html',
-  },
-  permissions: [
-    'sidePanel',
-    'contentSettings',
   ],
-  content_scripts: [{
-    js: ['src/content/main.tsx'],
-    matches: ['https://*/*'],
-  }],
   side_panel: {
-    default_path: 'src/sidepanel/index.html',
+    default_path: "src/sidepanel/index.html",
   },
-})
+});
