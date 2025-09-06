d9c5b5cb6d078db715b2e605c5da61d6e6a392a0
Mathious6 <mathious61@gmail.com>
2025-09-06 18:44:10 +0000

    feat: implement Header component and remove HelloWorld component

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import discordLogo from "@/assets/discord.svg";
+import githubLogo from "@/assets/github.svg";
+
+const DISCORD_URL = "https://discord.com/channels/@me";
+const GITHUB_URL = "https://github.com/Mathious6/discord-link-opener";
+
+export default function Header() {
+  return (
+    <header>
+      <a href={DISCORD_URL} target="_blank" rel="noreferrer">
+        <img src={discordLogo} className="logo discord" alt="Discord logo" />
+      </a>
+      <a href={GITHUB_URL} target="_blank" rel="noreferrer">
+        <img src={githubLogo} className="logo github" alt="GitHub logo" />
+      </a>
+      <h1>Discord link-opener</h1>
+    </header>
+  );
+}
--- src/sidepanel/App.tsx
+++ src/sidepanel/App.tsx
@@ -1,27 +1,6 @@
-import discordLogo from "@/assets/discord.svg";
-import githubLogo from "@/assets/github.svg";
-import HelloWorld from "@/components/HelloWorld";
+import Header from "@/components/Header";
 import "./App.css";
 
 export default function App() {
-  return (
-    <div>
-      <a
-        href="https://discord.com/channels/@me"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={discordLogo} className="logo discord" alt="Discord logo" />
-      </a>
-      <a
-        href="https://github.com/Mathious6/discord-link-opener"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={githubLogo} className="logo github" alt="GitHub logo" />
-      </a>
-      <h1>Discord link-opener</h1>
-      <HelloWorld />
-    </div>
-  );
+  return <Header />;
 }
