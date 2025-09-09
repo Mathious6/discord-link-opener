3fd5059aae37b96c6418f6f331ff8dfc87688ec7
Mathious6 <mathious61@gmail.com>
2025-09-09 02:11:33 +0000

    feat: add Button, Input, and Label components; update TaskSettings to utilize new components and integrate Radix UI label

--- src/components/TaskSettings.tsx
+++ src/components/TaskSettings.tsx
@@ -1,4 +1,8 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
+import { PlayIcon, Square } from "lucide-react";
 
 export default function TaskSettings() {
   return (
@@ -7,16 +11,30 @@ export default function TaskSettings() {
         <div className="container flex items-center justify-between py-4">
           <h2 className="text-lg font-semibold">Task settings</h2>
           <div className="flex items-center space-x-2">
-            <h3>Start</h3>
-            <h3>Stop</h3>
+            <Button
+              size="icon"
+              className="bg-green-500/20 text-green-500 hover:bg-green-500/40 hover:text-green-600"
+            >
+              <PlayIcon />
+            </Button>
+            <Button
+              size="icon"
+              className="bg-red-500/20 text-red-500 hover:bg-red-500/40 hover:text-red-600"
+            >
+              <Square />
+            </Button>
           </div>
         </div>
         <Separator />
-        <div className="container h-full py-6">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-          excepturi culpa beatae, impedit temporibus sint ut doloremque sunt
-          molestiae voluptatum nostrum cupiditate iste nulla harum, ab doloribus
-          dignissimos accusantium! Dignissimos?
+        <div className="container grid h-full w-full items-center gap-3 py-6">
+          <Label htmlFor="regex">Regex filter</Label>
+          <Input
+            type="text"
+            id="regex"
+            placeholder="^https?:\/\/(?:[\w-]+\.)*ldlc\.com(?:\/\S*)?$"
+          />
+          <Label htmlFor="delay">Open delay (ms)</Label>
+          <Input type="number" id="delay" placeholder="1000" />
         </div>
       </div>
     </div>
