f1520db345696079bc10009c1c47e9d9a6c20b45
Mathious6 <mathious61@gmail.com>
2025-09-16 02:47:57 +0000

    chore: update ESLint configuration to include TypeScript and improve global settings

--- eslint.config.ts
+++ eslint.config.ts
@@ -9,40 +9,43 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
+  tseslint.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    files: ["**/*.{ts,mts,cts,tsx}"],
     plugins: { js, react, tailwindcss: tailwind },
     extends: ["js/recommended"],
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        chrome: "readonly",
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
     },
-    settings: { react: { version: "detect" } },
+    settings: {
+      react: { version: "detect" },
+      tailwindcss: { config: "./tailwind.config.js" },
+    },
     rules: {
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
     },
   },
-
-  // typescript
-  tseslint.configs.recommended,
-
-  // json
   {
     files: ["**/*.json"],
     plugins: { json },
     language: "json/json",
     extends: ["json/recommended"],
   },
-
-  // markdown
   {
     files: ["**/*.md"],
     plugins: { markdown },
     language: "markdown/commonmark",
     extends: ["markdown/recommended"],
   },
-
-  // prettier (compatibility)
   eslintConfigPrettier,
 ]);
